147350f379804403119ed63be645eec577aa698a
Jeff Horwitz <jeff@smashing.org>
2023-02-13 09:31:12 -0500

    initial commit

---
+++ go.mod
@@ -0,0 +1,20 @@
+module github.com/jhorwitz75/stu
+
+go 1.19
+
+require (
+	github.com/gdamore/encoding v1.0.0 // indirect
+	github.com/gdamore/tcell/v2 v2.5.3 // indirect
+	github.com/gen2brain/beeep v0.0.0-20220909211152-5a9ec94374f6 // indirect
+	github.com/go-toast/toast v0.0.0-20190211030409-01e6764cf0a4 // indirect
+	github.com/godbus/dbus/v5 v5.1.0 // indirect
+	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
+	github.com/mattn/go-runewidth v0.0.13 // indirect
+	github.com/nu7hatch/gouuid v0.0.0-20131221200532-179d4d0c4d8d // indirect
+	github.com/rivo/tview v0.0.0-20230208211350-7dfff1ce7854 // indirect
+	github.com/rivo/uniseg v0.4.2 // indirect
+	github.com/tadvi/systray v0.0.0-20190226123456-11a2b8fa57af // indirect
+	golang.org/x/sys v0.0.0-20220319134239-a9b59b0215f8 // indirect
+	golang.org/x/term v0.0.0-20210220032956-6a3ed077a48d // indirect
+	golang.org/x/text v0.3.7 // indirect
+)
---
+++ main.go
@@ -0,0 +1,202 @@
+package main
+
+import (
+    "encoding/csv"
+    "flag"
+    "fmt"
+    "io/ioutil"
+    "log"
+    "os"
+    "strconv"
+    "strings"
+
+    "github.com/gdamore/tcell/v2"
+    "github.com/gen2brain/beeep"
+    "github.com/rivo/tview"
+)
+
+var app = tview.NewApplication()
+var pages = tview.NewPages()
+var form = tview.NewForm()
+var splitString string = ""
+var splitCols int = 0
+
+func swapColumns(table *tview.Table, leftCol int, rightCol int){
+    for row := 0; row < table.GetRowCount(); row++ {
+        rightCell := table.GetCell(row, rightCol)
+        temp := table.GetCell(row, leftCol)
+        table.SetCell(row, rightCol, temp)
+        table.SetCell(row, leftCol, rightCell)
+    }
+}
+
+func splitColumnByString(table *tview.Table, s string) {
+    _, selectedCol := table.GetSelection()
+    sourceCol := selectedCol
+    for row := 0; row < table.GetRowCount(); row++ {
+        // get cell contents to split
+        cell := table.GetCell(row, sourceCol)
+
+        // split cell contents and determine # of new columns
+        var parts []string
+        if splitCols == 0 {
+            parts = strings.Split(cell.Text, s)
+        } else {
+            parts = strings.SplitN(cell.Text, s, splitCols)
+            if len(parts) < splitCols {
+                beep()
+                break
+            }
+        }
+        ncols := len(parts)
+
+        // stop if we have nothing to split
+        if ncols == 1 {
+            beep()
+            break
+        }
+
+        // at first iteration, insert enough columns for new values
+        if row == 0 {
+            for i := 0; i < ncols-1; i++ {
+                table.InsertColumn(selectedCol)
+            }
+            // also reset the source column, which has now shifted
+            sourceCol = selectedCol + ncols - 1
+        }
+
+        // set cell contents to new split values
+        for i := 0; i < ncols; i++ {
+           col := selectedCol + i
+           table.SetCell(row, col, tview.NewTableCell(parts[i]))
+        }
+    }
+}
+
+func splitColumnByStringForm(table *tview.Table) *tview.Form {
+    form.AddInputField("Split on string", splitString, 60, nil, func (value string) {
+        splitString = value
+    })
+    form.AddInputField("Maximum fields (0 to disable)", "0", 2, nil, func (value string) {
+        i, err := strconv.Atoi(value)
+        if err != nil {
+            panic(err)
+        }
+        splitCols = i
+    })
+    form.AddButton("Split", func() {
+        splitColumnByString(table, splitString)
+        pages.SwitchToPage("Table")
+    })
+    form.AddButton("Cancel", func() {
+        pages.SwitchToPage("Table")
+    })
+
+    return form
+}
+
+func beep() {
+    beeep.Beep(440.0, 200)
+}
+
+func main() {
+    var inputText string
+
+    flag.Parse()
+
+    inputFile := flag.Arg(0)
+    if inputFile == "" {
+        inputText = ""
+    } else {
+        content, err := ioutil.ReadFile(inputFile)
+        if err != nil {
+            log.Fatal(err)
+        }
+
+        inputText = strings.TrimRight(string(content), "\r\n")
+    }
+
+    table := tview.NewTable().SetBorders(true)
+    table.SetCell(0, 0, tview.NewTableCell(inputText))
+    table.SetSelectable(true, true)
+
+    statusBar := tview.NewTextView().
+        SetTextColor(tcell.ColorRed).
+        SetText("Hello world!")
+
+    // BEGIN TEST
+    //form.Clear(true)
+    //splitColumnByStringForm()
+    // END TEST
+
+    var flex = tview.NewFlex().SetDirection(tview.FlexRow).
+        AddItem(table, 0, 1, true).
+        AddItem(statusBar, 1, 0, false)
+
+    app.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+        switch rune := event.Rune(); rune {
+        case 'q':
+            app.Stop()
+        case 'L':
+            row, col := table.GetSelection()
+            if col > 0 {
+                swapColumns(table, col-1, col)
+                table.Select(row, col-1)
+            } else {
+                beep()
+            }
+            return nil
+        case 'R':
+            row, col := table.GetSelection()
+            if col < table.GetColumnCount()-1 {
+                swapColumns(table, col, col+1)
+                table.Select(row, col+1)
+            } else {
+                beep()
+            }
+            return nil
+        case 's':
+            form.Clear(true)
+            splitColumnByStringForm(table)
+            pages.SwitchToPage("SplitColumnByStringForm")
+            return nil
+        case 'w':
+            f, err := os.Create(inputFile)
+            if err != nil {
+                panic(err)
+            }
+            defer f.Close()
+
+            w := csv.NewWriter(f)
+            defer w.Flush()
+
+            ncols := table.GetColumnCount()
+            for row := 0; row < table.GetRowCount(); row++ {
+                record := make([]string, ncols)
+                for col := 0; col < ncols; col++ {
+                    record[col] = table.GetCell(row, col).Text
+                }
+                if err := w.Write(record); err != nil {
+                    panic(err)
+                }
+            }
+            app.Stop()
+        }
+
+        // update status bar
+        statusText := fmt.Sprintf("Hi there. Key code is %d", event.Rune())
+        statusBar.SetText(statusText)
+
+        return event
+    })
+
+    pages.AddPage("Table", flex, true, true)
+    pages.AddPage("SplitColumnByStringForm", form, true, false)
+
+    app.SetFocus(flex)
+
+    if err := app.SetRoot(pages, true).
+        EnableMouse(true).Run(); err != nil {
+        panic(err)
+    }
+}
