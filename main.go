d94017428d0926ed5079394cf5d21feafb920597
Jeff Horwitz <jeff@smashing.org>
2023-02-19 11:14:43 -0500

    format using gofmt

--- main.go
+++ main.go
@@ -1,19 +1,19 @@
 package main
 
 import (
-    "bufio"
-    "encoding/csv"
-    "flag"
-    "fmt"
-    "io"
-    "log"
-    "os"
-    "strconv"
-    "strings"
+	"bufio"
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strconv"
+	"strings"
 
-    "github.com/gdamore/tcell/v2"
-    "github.com/gen2brain/beeep"
-    "github.com/rivo/tview"
+	"github.com/gdamore/tcell/v2"
+	"github.com/gen2brain/beeep"
+	"github.com/rivo/tview"
 )
 
 var app = tview.NewApplication()
@@ -22,214 +22,214 @@ var form = tview.NewForm()
 var splitString string = ""
 var splitCols int = 0
 
-func swapColumns(table *tview.Table, leftCol int, rightCol int){
-    for row := 0; row < table.GetRowCount(); row++ {
-        rightCell := table.GetCell(row, rightCol)
-        temp := table.GetCell(row, leftCol)
-        table.SetCell(row, rightCol, temp)
-        table.SetCell(row, leftCol, rightCell)
-    }
+func swapColumns(table *tview.Table, leftCol int, rightCol int) {
+	for row := 0; row < table.GetRowCount(); row++ {
+		rightCell := table.GetCell(row, rightCol)
+		temp := table.GetCell(row, leftCol)
+		table.SetCell(row, rightCol, temp)
+		table.SetCell(row, leftCol, rightCell)
+	}
 }
 
 func splitColumnByString(table *tview.Table, s string) {
-    _, selectedCol := table.GetSelection()
-    sourceCol := selectedCol
-    for row := 0; row < table.GetRowCount(); row++ {
-        // get cell contents to split
-        cell := table.GetCell(row, sourceCol)
+	_, selectedCol := table.GetSelection()
+	sourceCol := selectedCol
+	for row := 0; row < table.GetRowCount(); row++ {
+		// get cell contents to split
+		cell := table.GetCell(row, sourceCol)
 
-        // split cell contents and determine # of new columns
-        var parts []string
-        if splitCols == 0 {
-            parts = strings.Split(cell.Text, s)
-        } else {
-            parts = strings.SplitN(cell.Text, s, splitCols)
-            if len(parts) < splitCols {
-                beep()
-                break
-            }
-        }
-        ncols := len(parts)
+		// split cell contents and determine # of new columns
+		var parts []string
+		if splitCols == 0 {
+			parts = strings.Split(cell.Text, s)
+		} else {
+			parts = strings.SplitN(cell.Text, s, splitCols)
+			if len(parts) < splitCols {
+				beep()
+				break
+			}
+		}
+		ncols := len(parts)
 
-        // stop if we have nothing to split
-        if ncols == 1 {
-            beep()
-            break
-        }
+		// stop if we have nothing to split
+		if ncols == 1 {
+			beep()
+			break
+		}
 
-        // at first iteration, insert enough columns for new values
-        if row == 0 {
-            for i := 0; i < ncols-1; i++ {
-                table.InsertColumn(selectedCol)
-            }
-            // also reset the source column, which has now shifted
-            sourceCol = selectedCol + ncols - 1
-        }
+		// at first iteration, insert enough columns for new values
+		if row == 0 {
+			for i := 0; i < ncols-1; i++ {
+				table.InsertColumn(selectedCol)
+			}
+			// also reset the source column, which has now shifted
+			sourceCol = selectedCol + ncols - 1
+		}
 
-        // set cell contents to new split values
-        for i := 0; i < ncols; i++ {
-           col := selectedCol + i
-           table.SetCell(row, col, tview.NewTableCell(parts[i]))
-        }
-    }
+		// set cell contents to new split values
+		for i := 0; i < ncols; i++ {
+			col := selectedCol + i
+			table.SetCell(row, col, tview.NewTableCell(parts[i]))
+		}
+	}
 }
 
 func splitColumnByStringForm(table *tview.Table) *tview.Form {
-    form.AddInputField("Split on string", splitString, 60, nil, func (value string) {
-        splitString = value
-    })
-    form.AddInputField("Maximum fields (0 to disable)", "0", 2, nil, func (value string) {
-        i, err := strconv.Atoi(value)
-        if err != nil {
-            panic(err)
-        }
-        splitCols = i
-    })
-    form.AddButton("Split", func() {
-        splitColumnByString(table, splitString)
-        pages.SwitchToPage("Table")
-    })
-    form.AddButton("Cancel", func() {
-        pages.SwitchToPage("Table")
-    })
+	form.AddInputField("Split on string", splitString, 60, nil, func(value string) {
+		splitString = value
+	})
+	form.AddInputField("Maximum fields (0 to disable)", "0", 2, nil, func(value string) {
+		i, err := strconv.Atoi(value)
+		if err != nil {
+			panic(err)
+		}
+		splitCols = i
+	})
+	form.AddButton("Split", func() {
+		splitColumnByString(table, splitString)
+		pages.SwitchToPage("Table")
+	})
+	form.AddButton("Cancel", func() {
+		pages.SwitchToPage("Table")
+	})
 
-    return form
+	return form
 }
 
 func beep() {
-    beeep.Beep(440.0, 200)
+	beeep.Beep(440.0, 200)
 }
 
 func readCSV(table *tview.Table, filename string) {
-    file, err := os.Open(filename)
-    if err != nil {
-        log.Fatal(err)
-    }
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    defer file.Close()
+	defer file.Close()
 
-    csvReader := csv.NewReader(file)
-    var rowNum int
-    for {
-        row, err := csvReader.Read()
-        if err == io.EOF {
-            break
-        }
-        if err != nil {
-            log.Fatal(err)
-        }
-        for colNum, field := range row {
-            table.SetCell(rowNum, colNum, tview.NewTableCell(field))
-        }
-        rowNum++
-    }
+	csvReader := csv.NewReader(file)
+	var rowNum int
+	for {
+		row, err := csvReader.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		for colNum, field := range row {
+			table.SetCell(rowNum, colNum, tview.NewTableCell(field))
+		}
+		rowNum++
+	}
 }
 
 func readPlainText(table *tview.Table, filename string) {
-    file, err := os.Open(filename)
-    if err != nil {
-        log.Fatal(err)
-    }
-    defer file.Close()
+	file, err := os.Open(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
 
-    scanner := bufio.NewScanner(file)
-    scanner.Split(bufio.ScanLines)
+	scanner := bufio.NewScanner(file)
+	scanner.Split(bufio.ScanLines)
 
-    var numLines int
-    for scanner.Scan() {
-        table.SetCell(numLines, 0, tview.NewTableCell(scanner.Text()))
-        numLines++
-    }
+	var numLines int
+	for scanner.Scan() {
+		table.SetCell(numLines, 0, tview.NewTableCell(scanner.Text()))
+		numLines++
+	}
 }
 
 func main() {
-    flag.Parse()
+	flag.Parse()
 
-    inputFile := flag.Arg(0)
+	inputFile := flag.Arg(0)
 
-    table := tview.NewTable().SetBorders(true)
-    table.SetSelectable(true, true)
+	table := tview.NewTable().SetBorders(true)
+	table.SetSelectable(true, true)
 
-    if inputFile == "" {
-        table.SetCell(0, 0, tview.NewTableCell(""))
-    } else if strings.HasSuffix(inputFile, "csv") {
-        readCSV(table, inputFile)
-    } else {
-        readPlainText(table, inputFile)
-    }
+	if inputFile == "" {
+		table.SetCell(0, 0, tview.NewTableCell(""))
+	} else if strings.HasSuffix(inputFile, "csv") {
+		readCSV(table, inputFile)
+	} else {
+		readPlainText(table, inputFile)
+	}
 
-    statusBar := tview.NewTextView().
-        SetTextColor(tcell.ColorRed).
-        SetText("Hello world!")
+	statusBar := tview.NewTextView().
+		SetTextColor(tcell.ColorRed).
+		SetText("Hello world!")
 
-    var flex = tview.NewFlex().SetDirection(tview.FlexRow).
-        AddItem(table, 0, 1, true).
-        AddItem(statusBar, 1, 0, false)
+	var flex = tview.NewFlex().SetDirection(tview.FlexRow).
+		AddItem(table, 0, 1, true).
+		AddItem(statusBar, 1, 0, false)
 
-    app.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
-        switch rune := event.Rune(); rune {
-        case 'q':
-            app.Stop()
-        case 'L':
-            row, col := table.GetSelection()
-            if col > 0 {
-                swapColumns(table, col-1, col)
-                table.Select(row, col-1)
-            } else {
-                beep()
-            }
-            return nil
-        case 'R':
-            row, col := table.GetSelection()
-            if col < table.GetColumnCount()-1 {
-                swapColumns(table, col, col+1)
-                table.Select(row, col+1)
-            } else {
-                beep()
-            }
-            return nil
-        case 's':
-            form.Clear(true)
-            splitColumnByStringForm(table)
-            pages.SwitchToPage("SplitColumnByStringForm")
-            return nil
-        case 'w':
-            f, err := os.Create(inputFile)
-            if err != nil {
-                panic(err)
-            }
-            defer f.Close()
+	app.SetInputCapture(func(event *tcell.EventKey) *tcell.EventKey {
+		switch rune := event.Rune(); rune {
+		case 'q':
+			app.Stop()
+		case 'L':
+			row, col := table.GetSelection()
+			if col > 0 {
+				swapColumns(table, col-1, col)
+				table.Select(row, col-1)
+			} else {
+				beep()
+			}
+			return nil
+		case 'R':
+			row, col := table.GetSelection()
+			if col < table.GetColumnCount()-1 {
+				swapColumns(table, col, col+1)
+				table.Select(row, col+1)
+			} else {
+				beep()
+			}
+			return nil
+		case 's':
+			form.Clear(true)
+			splitColumnByStringForm(table)
+			pages.SwitchToPage("SplitColumnByStringForm")
+			return nil
+		case 'w':
+			f, err := os.Create(inputFile)
+			if err != nil {
+				panic(err)
+			}
+			defer f.Close()
 
-            w := csv.NewWriter(f)
-            defer w.Flush()
+			w := csv.NewWriter(f)
+			defer w.Flush()
 
-            ncols := table.GetColumnCount()
-            for row := 0; row < table.GetRowCount(); row++ {
-                record := make([]string, ncols)
-                for col := 0; col < ncols; col++ {
-                    record[col] = table.GetCell(row, col).Text
-                }
-                if err := w.Write(record); err != nil {
-                    panic(err)
-                }
-            }
-            app.Stop()
-        }
+			ncols := table.GetColumnCount()
+			for row := 0; row < table.GetRowCount(); row++ {
+				record := make([]string, ncols)
+				for col := 0; col < ncols; col++ {
+					record[col] = table.GetCell(row, col).Text
+				}
+				if err := w.Write(record); err != nil {
+					panic(err)
+				}
+			}
+			app.Stop()
+		}
 
-        // update status bar
-        statusText := fmt.Sprintf("Hi there. Key code is %d", event.Rune())
-        statusBar.SetText(statusText)
+		// update status bar
+		statusText := fmt.Sprintf("Hi there. Key code is %d", event.Rune())
+		statusBar.SetText(statusText)
 
-        return event
-    })
+		return event
+	})
 
-    pages.AddPage("Table", flex, true, true)
-    pages.AddPage("SplitColumnByStringForm", form, true, false)
+	pages.AddPage("Table", flex, true, true)
+	pages.AddPage("SplitColumnByStringForm", form, true, false)
 
-    app.SetFocus(flex)
+	app.SetFocus(flex)
 
-    if err := app.SetRoot(pages, true).
-        EnableMouse(true).Run(); err != nil {
-        panic(err)
-    }
+	if err := app.SetRoot(pages, true).
+		EnableMouse(true).Run(); err != nil {
+		panic(err)
+	}
 }
